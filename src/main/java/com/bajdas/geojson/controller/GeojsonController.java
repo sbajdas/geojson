3b01159268c401c630cb1c3c98ac0e025af6da33
Slawomir Bajdas <Slawomir_Bajdas@epam.com>
2019-03-06 15:13:02 +0100

    Logowanie zapytan pÃ³ki cologuje sie wszystko z INFO

--- src/main/java/com/bajdas/geojson/controller/GeojsonController.java
+++ src/main/java/com/bajdas/geojson/controller/GeojsonController.java
@@ -1,14 +1,19 @@
 package com.bajdas.geojson.controller;
 
 import com.bajdas.geojson.service.GeojsonService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
 public class GeojsonController {
     private final GeojsonService geojsonService;
+    private Logger logger = LoggerFactory.getLogger(GeojsonController.class);
 
     @Autowired
     public GeojsonController(GeojsonService geojsonService) {
@@ -16,7 +21,10 @@ public class GeojsonController {
     }
 
     @RequestMapping("/geojson/{cityName}")
-    String getGeoJsonFromCityName(@PathVariable String cityName) {
+    String getGeoJsonFromCityName(HttpServletRequest request, @PathVariable String cityName) {
+
+        String userName = request.getUserPrincipal().getName();
+        logger.info(String.format("%s : %s", userName,cityName));
         return geojsonService.lookForGeoJson(cityName);
     }
 }
